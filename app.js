a398d993f0ffbf33d5784b8bb96ce7d8c4a0550e
Yasmin Magalhaes <yasminmagalhaes@MacBook-Pro-de-Yasmin.local>
2020-12-15 15:49:59 -0300

    Projeto final workshop

---
+++ app.js
@@ -0,0 +1,11 @@
+let fim = document.getElementById('final')
+            fim.setAttribute("style", "padding: 30px")
+
+            let cont1 = document.getElementById('container1')
+            cont1.innerText = "Criado por Yasmin"
+            cont1.setAttribute("style", "text-align: center")
+
+            let cont2 = document.getElementById('container2')
+            cont2.innerHTML = "Acessado em " + Date()
+            cont2.setAttribute("style", "text-align: center")
+            
